2851a261c1c543c381b2d9115b30da3827efb461
utusemisiki <monsuto.mura@gmail.com>
2025-01-21 23:20:06 +0900

    コメントを追加
    コメントを追加しています
    現在はデータに保存しておらずに新しいコメントが来たら上書きになっているため今後は、データの保存が必要

---
+++ rennsyuu/rennsyuu/include.php
@@ -0,0 +1,28 @@
+<?php
+$hobby1 = "ゲームが好きです";
+$hobby2 = ["大乱闘スマッシュブラザーズ","eFootball","ヘブンズバーンレッド","ウマ娘"];
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/css1.css"> <!-- CSSファイル -->
+    <title>趣味一覧</title>
+</head>
+<body>
+<section  >
+<div class="#intro">
+        <h2>趣味、好きなこと</h2>
+        
+   
+        <p><?php echo $hobby1; ?></p>
+        <h3>主にしているゲーム</h3> 
+    <?php foreach($hobby2 as $index =>$hobbylist ){
+    echo $hobbylist."<br>";
+    } 
+    ?>
+</div>
+</section>
+</body>
+</html>
